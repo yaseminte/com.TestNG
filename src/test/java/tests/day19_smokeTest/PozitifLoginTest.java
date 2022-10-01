fa8ed52d04bd796e088a933b74e234655c0d7f27
Yasemin Turker <yturker89@gmail.com>
2022-10-01 08:45:46 +0300

    01.10.2022

---
+++ src/test/java/tests/day19_smokeTest/PozitifLoginTest.java
@@ -0,0 +1,6 @@
+package tests.day19_smokeTest;
+
+public class PozitifLoginTest {
+
+
+}
--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -17,8 +17,23 @@ public class Driver {
     Driver class'indan static method'lar kullanarak
     driver olusturup, ilgili ayarlarin yapilmasi
     ve en sonda driver'in kapatilmasi tercih edilmistir.
+
+    POM'da Driver class'indaki getDriver()'in obje olusturularak kullanilmasini
+    engellemek icin
+    Singleton pattern kullanimi benimsenmistir.
+
+    Singleton Pattern : tekli kullanim, bir class'in farkli class'lardan
+    obje olusturularak kullanimini engellemek icin kullanilir.
+
+    Bunu saglamak icin yapmamiz gereken sey oldukca basit
+    obje olusturmak icin kullanilan constructor'i private yaptiginizda
+    baska class'larda Driver class'indan obje olusturulmasi mumkun OLAMAZ
      */
-    static WebDriver driver;
+    private Driver(){
+
+    }
+
+    public static WebDriver driver;
 
     public static WebDriver getDriver() {
 
